6d4d1d21569c92bb741d1be5e1fb8ea41bdfaf1a
wuxuwei <172498667@qq.com>
2022-04-24 01:35:14 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,24 @@ export default {
 }
 </script>
 
-<style>
+<style type="text/css">
+body,html{
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333333;
+  background-color: rgb(143, 184, 171);
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cover_bg">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cover_bg{
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/cover.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: #f9f9f9;
+}
+</style>
