a53e12eed00914fb2499d31d51f0b78467986d04
wuxuwei <172498667@qq.com>
2022-04-23 13:16:20 +0800

    init

---
+++ src/components/Apply.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <p>报名页面</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Apply',
+  data () {
+    return {
+      msg: 'Welcome to Watsons App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
