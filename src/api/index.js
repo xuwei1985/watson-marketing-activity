d6208755ddf2d5dcbc46339f8f2e4daeb4a4ac97
wujie <wujie892873032@163.com>
2022-04-26 22:27:58 +0800

    报名

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from '@/assets/js/axios'
 
 const postApply = (query) => {
-  return axios.post('/applyer/list', query)
+  return axios.post('/applyer', query)
 }
 
 const getApplyList = (query) => {
