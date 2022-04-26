2ff3681f71f51cc3ae08397ac4587b2ab2484859
wuxuwei <172498667@qq.com>
2022-04-26 15:18:19 +0800

    no message

--- src/views/ApplyView.vue
+++ src/views/ApplyView.vue
@@ -3,10 +3,10 @@
        <div class="ap_logo"><img src="../assets/img/cover_logo.png"/></div>
        <div class="ap_box">
         <ul>
-          <li><input class="input_name magictime boingInUp" type="text" :v-model="formData.name" maxlength="20" @focus="changeBg" /></li>
-          <li><input class="input_mobile magictime boingInUp" type="tel" :v-model="formData.mobile"  maxlength="11" style="animation-delay: 0.1s;" @focus="changeBg" /></li>
-          <li><input class="input_number magictime boingInUp" type="text" :v-model="formData.number" maxlength="9" style="animation-delay: 0.2s;" @focus="changeBg" /></li>
-          <li><input class="input_city magictime boingInUp" type="text" :v-model="formData.city" maxlength="4" style="animation-delay: 0.3s;" @focus="changeBg" /></li>
+          <li><input class="input_name magictime boingInUp" type="text" placeholder=" " :v-model="formData.name" maxlength="20" /></li>
+          <li><input class="input_mobile magictime boingInUp" type="tel" placeholder=" " :v-model="formData.mobile"  maxlength="11" style="animation-delay: 0.1s;" /></li>
+          <li><input class="input_number magictime boingInUp" type="text" placeholder=" " :v-model="formData.number" maxlength="9" style="animation-delay: 0.2s;" /></li>
+          <li><input class="input_city magictime boingInUp" type="text" placeholder=" " :v-model="formData.city" maxlength="4" style="animation-delay: 0.3s;" /></li>
           <li><button class="btn_channel magictime boingInUp" style="animation-delay: 0.4s;" @click="selectChannel" /></li>
         </ul>
        </div>
@@ -85,7 +85,7 @@ export default {
       border: 1px solid #cfcfcf;
       border-radius: 1.8vw;
       text-align: center;
-      font-size: 2.4em;
+      font-size: 2.2em;
       padding: 0.2vw 4vw 0.5vw 3.7vw;
       box-sizing: border-box;
       color:#5f3ba2;
@@ -93,20 +93,22 @@ export default {
     }
     input:focus{
       outline: none;
+    }
+    .input_name,.input_mobile,.input_number,.input_city{
       background-image: url('../assets/img/apply_input_bg.png');
     }
   }
 
-  .input_name{
+  .input_name:placeholder-shown {
     background-image: url('../assets/img/apply_input_name.png');
   }
-  .input_mobile{
+  .input_mobile:placeholder-shown{
     background-image: url('../assets/img/apply_input_mobile.png');
   }
-  .input_number{
+  .input_number:placeholder-shown{
     background-image: url('../assets/img/apply_input_number.png');
   }
-  .input_city{
+  .input_city:placeholder-shown{
     background-image: url('../assets/img/apply_input_city.png');
   }
   .btn_channel{
